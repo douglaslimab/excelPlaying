0aa3a975a752471e7f3d295175452402a5a1de28
douglaslimab <douglaslimab@gmail.com>
2022-03-07 23:56:40 +0000

    serial test

--- serial_test.py
+++ serial_test.py
@@ -2,23 +2,20 @@ import serial
 import random
 
 uart_1 = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
+data = 'a'
 
 
 def read():
     return uart_1.readline()
 
 
-def write(data):
-    uart_1.write(bytes(data, 'utf-8'))
+def write(data_tx):
+    uart_1.write(str(data_tx, 'utf-8'))
 
 
-while(True):
-    output = random.randint(0, 10)
-    write(output)
-    res = read()
+while True:
+    write(data)
+    data = read()
 
-    if(res == output):
-        print(f'Success {res} == {output}')
-
-    else:
-        print(f'Fail {res} != {output}')
+    if str(data, 'utf-8') != '':
+        print(f"{str(data, 'utf-8')}")
